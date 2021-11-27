5dbe83ceedc3e7eb402eaab868ff17f18f798a14
yusugiura <19901220dev@gmail.com>
2021-11-28 01:59:43 +0900

    test: linter

--- src/world.ts
+++ src/world.ts
@@ -1,13 +1,16 @@
 export default class World {
-    message: string;
+  message: string
 
-    constructor(message:string) {
-        this.message = message
-    }
+  constructor(message: string) {
+    this.message = message
+  }
 
-    public sayHello(elem: HTMLElement | null) {
-        if (elem) {
-            elem.innerText = this.message
-        }
+  public sayHello(elem: HTMLElement | null) {
+    if (elem) {
+      elem.innerText = this.message
     }
+  }
+
+
+  
 }
